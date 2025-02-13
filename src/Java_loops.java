b8ffad6bba4d224502fb6217c6a1bea5fba0d161
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-02-13 12:59:47 +0530

    init commit

---
+++ src/Java_loops.java
@@ -0,0 +1,11 @@
+public class Java_loops {
+
+        public static void main(String[] args) {
+            // Using a for loop to print numbers from 1 to 10
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i + " "); // Printing numbers in a single line
+            };
+        };
+
+
+};

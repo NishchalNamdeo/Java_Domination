0b7452975a15fcefeab3993b3a75caae1c5826a0
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-03-15 20:41:21 +0530

    init commit

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -1,4 +1,88 @@
 package arrays;
 
+import java.util.Scanner;
+
+
 public class Arrays {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+
+
+
+//      SUM of Array element
+//        System.out.println("give me size of array");
+//        int size = sc.nextInt();
+//        int arr[] = new int[size];
+//        int sum = 0;
+//        for (int i = 0; i <arr.length ; i++) {
+//            arr[i] = sc.nextInt();
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//        System.out.println((double) sum/size);
+
+
+
+//    Greatest in Array
+//        int[] arr = {10, 5, 20, 38, 8};
+//        int great = 0;
+//        for (int i =1; i < arr.length; i++) {
+//            if (arr[i] > arr[great]) {
+//                great = i;
+//            }
+//            System.out.println("greates = " + arr[great] + " found at =" + great);
+//        }
+
+
+//    second greatest in array
+//        int [] arr = {10, 30, 8, 80, 40};
+//        int first = Math.max(arr[0], arr[1]);
+//        int sec = Math.min(arr[0], arr[1]);
+//        for (int i = 2; i <arr.length ; i++) {
+//            if( arr[i] > first){
+//                sec = first;
+//                first = arr[i];
+//            }else if(arr[i] > sec){
+//                sec = arr[i];
+//            }
+//        }
+//        System.out.println(sec);
+
+
+//    Is array sorted ( line se sorted  ho)
+//        int[] arr = {10, 16, 30};
+//        boolean isSorted = true;
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i-1] > arr[i]) {
+//                isSorted = false;
+//                break;
+//            }
+//        }
+//        System.out.println(isSorted);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
+
+

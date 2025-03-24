4c5e498a847414c1dc52e74d5ad5ecce118a9038
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-03-24 15:23:53 +0530

    init commit

--- src/arrays/Questarray.java
+++ src/arrays/Questarray.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 //import java.util.Scanner;
 
 
-public class Questarray {
-    public static void main(String[] args) {
+//public class Questarray {
+//    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
 
 
@@ -107,40 +107,82 @@ public class Questarray {
 //        }
 //        System.out.println(Arrays.toString(temp));
 
-        int[] arr = {1, 2, 3, 4, 5};
-        int n = arr.length;
-        int i = 0, j = n - 1;
-        while (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-        System.out.println(Arrays.toString(arr));
-        
+//        int[] arr = {1, 2, 3, 4, 5};
+//        int n = arr.length;
+//        int i = 0, j = n - 1;
+//        while (i < j) {
+//            int temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+//            i++;
+//            j--;
+//        }
+//        System.out.println(Arrays.toString(arr));
 
 
+//    }
+//}
 
 
+//liner search Question;
+
+//public class Questarray {
+//    public static int linearsearch(int arr[], int key){
+//        for (int i = 0; i <arr.length ; i++) {
+//            if (key == arr[i]){
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+//    public static void main(String[] args) {
+//
+//   int arr[] = {29, 30, 59, 39};
+//   int key = 59;
+//        System.out.println(linearsearch(arr, key));
+//
+//
+//
+//    }
+//}
 
 
+//public class Questarray {
+//    public static int binearsearch(int arr[], int key) {
+//        int start = 0, end = arr.length - 1;
+//        while (start <= end) {
+//            int mid = (start + end) / 2;
+//            if (arr[mid] == key) {
+//                return mid;
+//            }
+//            if (arr[mid] > key) {
+//                return end = mid - 1;
+//            } else start = mid + 1;
+//        }
+//        return -1;
+//    }
+//    public static void main(String[] args) {
+//        int arr[] = {29, 34, 59, 39, 30};
+//        Arrays.sort(arr);
+//        int key = 39;
+//        System.out.println(binearsearch(arr, key));
+//    }
+//}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
-
-
+//bubble sort
+//public class Questarray {
+//    public static void main(String[] args) {
+//        int arr[] = {29, 34, 59, 39, 30};
+//        int n = arr.length;
+//        for (int i = 0; i < n-1; i++) {
+//            for (int j = 0; j < n-i-1; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    int temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+//    }
+//}

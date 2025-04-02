001ff20b68c1147faee3db41c37385fe3e80ea5a
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-04-02 23:03:58 +0530

    init commit

--- src/arrays/Classobject.java
+++ src/arrays/Classobject.java
@@ -1,7 +1,30 @@
 package arrays;
 
-public class Classobject {
-    public static void main(String[] args) {
 
+//Object Class Question -> pending
+//public class Classobject {
+//    public static void main(String[] args) {
+//
+//    }
+//}
+
+
+
+
+
+
+//ploymorphic -
+/* public class Classobject {
+    public static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+    public static double max(double a, double b) {
+        return a >= b ? a : b;
+    }
+
+    public static void main(String[] args)  {
+        System.out.println(Classobject.max(6.9 , 9.2));
+        System.out.println(Classobject.max(7 , 8));
     }
 }
+*/

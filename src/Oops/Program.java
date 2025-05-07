32604df85f2ec593e294769e8bcbd1f41967f73b
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-05-07 18:13:45 +0530

    init commit

---
+++ src/Oops/Program.java
@@ -0,0 +1,12 @@
+package Oops;
+public class Program {
+    public static void main(String[] agrs){
+     Getset s = new Getset("prachi", 43, "uit");
+     Getset s2 = new Getset("lachi", 54, "rgpv");
+
+        System.out.println(s);
+        s.setName("lovely");
+        System.out.println(s);
+
+    }
+}

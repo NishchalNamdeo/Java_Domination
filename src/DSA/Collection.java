c713985dfac811d7a0b30df3cdc449d8a53cf6d3
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-05-11 15:06:48 +0530

    init commit

--- src/DSA/Collection.java
+++ src/DSA/Collection.java
@@ -68,19 +68,94 @@ import java.util.*;
 
 
 //map - frequecy check karega
-public class Collection {
-    public static void main(String[] args) {
-        int arr[] = {1,1,2,3,4,5,6,6,66,4,44,56};
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int a : arr){
-            map.put(a , map.getOrDefault(a, 0)+1);
+//public class Collection {
+//    public static void main(String[] args) {
+//        int arr[] = {1,1,2,3,4,5,6,6,66,4,44,56};
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int a : arr){
+//            map.put(a , map.getOrDefault(a, 0)+1);
+//
+//        }
+//
+////        for printing
+//        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//            System.out.println("Element: " + entry.getKey() + ", Count: " + entry.getValue());
+//        }
+//
+//    }
+//}
 
-        }
 
-//        for printing
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            System.out.println("Element: " + entry.getKey() + ", Count: " + entry.getValue());
-        }
 
-    }
-}
+//Leetcode Question
+//1. two sum
+//class Collection {
+//        public int[] twoSum(int[] nums, int target) {
+//            for(int i=0; i<nums.length-1; i++){
+//                for(int j= i+1; j<nums.length; j++){
+//                    if(nums[i] + nums[j] == target){
+//                        return new int[] {i, j};
+//                    }
+//                }
+//
+//            }
+//            return null;
+//        }
+//}
+
+//second solution two sum ka
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for(int i=0; i<nums.length; i++){
+//        int comp = target - nums[i];
+//        if(map.containsKey(comp)){
+//            return new int[]{i, map.get(comp)};
+//        }else{
+//            map.put(nums[i], i);
+//        }
+//            }
+//        return null;
+
+
+
+//Ques2. Jewels and Stones = jewel ="aA" , stone="AaAAbb" ,  total output = 3 (jo match kr rha)
+//class Solution {
+//    public int numJewelsInStones(String jewels, String stones) {
+//        Set<Character> set = new HashSet<>();
+//        for (char j : jewels.toCharArray()){
+//          set.add(j);
+//        }
+//        int ans = 0;
+//        for (char s : stones.toCharArray()){
+//            if(set.contains(s)){
+//             ans++;
+//            }
+//        }
+//        return ans;
+//    }
+//}
+
+//second solution
+//  int ans = 0;
+//  for(char s: stones.toCharArray()){
+//    for(char j : jewels.toCharArrays()){
+//        if( s == j ){
+//            ans++;
+//        }
+//    }
+//    return ans;
+//}
+
+
+//checked the sentense is a pangram
+//class Solution {
+//    public boolean isPangram(String s) {
+//        s = s.toLowerCase();
+//        Set<Character> ch = new HashSet<>();
+//        for(char c: s.toCharArray()){
+//            if(c >= 'a' && c <= 'z'){
+//                ch.add(c);
+//            }
+//        }
+//        return ch.size() == 26;
+//    }
+//}

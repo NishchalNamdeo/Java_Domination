9428db83bc411a4281220e6321f6942b162d8f4f
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-05-15 23:29:17 +0530

    init commit

--- src/Recursion/Recurison.java
+++ src/Recursion/Recurison.java
@@ -13,15 +13,15 @@ package Recursion;
 //}
 
 
-public class Recurison {
-    public static int fib(int n) {
-        if (n <= 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(fib(6));
-    }
-
-}
+//public class Recurison {
+//    public static int fib(int n) {
+//        if (n <= 1)
+//            return n;
+//        return fib(n - 1) + fib(n - 2);
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(fib(6));
+//    }
+//
+//}

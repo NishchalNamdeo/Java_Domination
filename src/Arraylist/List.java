907bf83bf20d4afac8501fc22b5b314fd6579df4
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-05-20 11:19:44 +0530

    init commit

---
+++ src/Arraylist/List.java
@@ -0,0 +1,36 @@
+package Arraylist;
+import java.util.Arrays;
+
+
+//Ques -> reverse array k time
+//  public class List {
+//    public static void LeftRotation(int[] arr, int k) {
+//        int n = arr.length;
+//        k = k % n;
+//
+//        int[] temp = new int[n];
+//        int j = 0;
+//
+//        for (int i = k; i < n; i++) {
+//            temp[j++] = arr[i];
+//        }
+//
+//        for (int i = 0; i < k; i++) {
+//            temp[j++] = arr[i];
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = temp[i];
+//        }
+//        System.out.println(Arrays.toString(arr));
+//    }
+//    public static void main(String[] args){
+//        int arr[] = {1,2,3,4,5};
+//        int k = 2;
+//        LeftRotation(arr, k);
+//    }
+//}
+
+
+
+//Ques ->

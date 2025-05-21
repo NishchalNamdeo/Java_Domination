d0d03bae70a8fbf3114cac476d994ca2fb6f5549
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-05-21 10:28:14 +0530

    init commit

--- src/Arraylist/List.java
+++ src/Arraylist/List.java
@@ -1,7 +1,6 @@
 package Arraylist;
 import java.util.Arrays;
 
-
 //Ques -> reverse array k time
 //  public class List {
 //    public static void LeftRotation(int[] arr, int k) {
@@ -33,4 +32,63 @@ import java.util.Arrays;
 
 
 
-//Ques ->
+//Ques -> Consecutive ones in array
+//public class List{
+//    public static int Consecutive(int[] nums){
+//       int maxCount = 0;
+//       int count = 0;
+//
+//       for(int num :nums){
+//           if(num == 1){
+//               count++;
+//               maxCount = Math.max(maxCount, count);
+//           }
+//           else{
+//               count = 0;
+//           }
+//       }
+//        return maxCount;
+//    }
+//}
+
+
+//Ques -> Remove Duplicates from Sorted Array
+//public class List{
+//    public static int removeDuplicates(int[] nums){
+//        if(nums.length == 0) return 0;
+//
+//        int i = 0;
+//        for (int j = 1; j < nums.length ; j++) {
+//             if(nums[i] != nums[j]){
+//                 i++;
+//                 nums[i] = nums[j];
+//             }
+//        }
+//        return  i+1;
+//    }
+//}
+
+
+
+//Ques -> Marge Two sorted array
+//public class List {
+//    public static int[] merge(int[] a, int[] b) {
+//        Arrays.sort(a);
+//        Arrays.sort(b);
+//        int i = 0, j = 0, k = 0;
+//        int[] res = new int[a.length + b.length];
+//        while (i < a.length && j < b.length) {
+//            res[k++] = a[i] < b[j] ? a[i++] : b[j++];
+//        }
+//
+//        while (i<a.length) res[k++] = a[i++];
+//        while (j<b.length) res[k++] = b[j++];
+//
+//        return res;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] merged = merge(new int[]{5, 1, 9}, new int[]{3, 6, 2});
+//        System.out.println(Arrays.toString(merged));
+//    }
+//}

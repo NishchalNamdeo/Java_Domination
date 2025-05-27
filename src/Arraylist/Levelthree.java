fa54c8a84b7caab107ed7b0f1f1e075fd9b06371
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-05-27 11:27:38 +0530

    init commit

--- src/Arraylist/Levelthree.java
+++ src/Arraylist/Levelthree.java
@@ -1,5 +1,7 @@
 package Arraylist;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //Ques1 ->  Find Pivot Index
 //public class Levelthree {
@@ -71,3 +73,80 @@ package Arraylist;
 //}
 
 
+
+
+//Ques5. Marge intervals
+//class Levelthree{
+//    public int[][] merge(int[][] intervals){
+//       Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+//       if(intervals.length == 1){
+//           return intervals;
+//       }
+//       List <int[]> list = new ArrayList<>();
+//       for(int[] interval : intervals){
+//           int size = list.size();
+//           if(size == 0 || list.get(size-1)[1] < interval[0]){
+//               list.add(interval);
+//           }else{
+//               list.get(size-1)[1] = Math.max(list.get(size-1)[1], interval[1]);
+//           }
+//       }
+//
+//        int[][] ans = new int[list.size()][];
+//        for(int i=0; i<list.size(); i++){
+//            ans[i] = list.get(i);
+//        }
+//        return ans;
+//    }
+//}
+
+
+//Ques6. -> 3Sum
+//public class Levelthree {
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> ans = new ArrayList<>();
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length - 2; i++) {
+//            if (i != 0 && nums[i] == nums[i - 1]) continue;
+//            int j = i + 1;
+//            int k = nums.length - 1;
+//            while (j < k) {
+//                int sum = nums[i] + nums[j] + nums[k];
+//                if (sum == 0) {
+//                    ans.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+//                    while (j < k && nums[j] == nums[j - 1]) j++;
+//                    while (j < k && nums[k] == nums[k + 1]) k--;
+//                } else if (sum < 0) {
+//                    j++;
+//                } else {
+//                    k--;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+//}
+
+
+//Ques7. -> Trapping Rain Water
+//public class Levelthree {
+//    public int trap(int[] height) {
+//        int maxR[] = new int[height.length];
+//        int max = height[height.length - 1];
+//        maxR[maxR.length - 1] = height[height.length - 1];
+//
+//        for (int i = maxR.length - 2; i >= 0; i--) {
+//            maxR[i] = max;
+//            max = Math.min(height[i], max);
+//        }
+//
+//        int maxL = height[0];
+//        int water = 0;
+//        for (int i = 0; i < height.length; i++) {
+//            if ((Math.min(maxL, maxR[i]) - height[i]) > 0)
+//                water += Math.min(maxL, maxR[i] - height[i]);
+//            water += Math.max(maxL, height[i]);
+//        }
+//        return water;
+//    }
+//}

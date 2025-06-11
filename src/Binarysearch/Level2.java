7d448a6e0a7e077c723d784e7e460755cecce53b
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-06-11 15:04:06 +0530

    init commit

--- src/Binarysearch/Level2.java
+++ src/Binarysearch/Level2.java
@@ -1,5 +1,5 @@
 package Binarysearch;
-
+import java.util.Arrays;
 
 //Ques1. -> Peak Index in a Mountain Array
 //public class Level2 {
@@ -19,7 +19,37 @@ package Binarysearch;
 //}
 
 
-//ques2. ->
+//ques2. -> Search in Rotated Sorted Array
+//public class Level2{
+//    public int search(int[] nums, int target){
+//        int start = 0;
+//        int end = nums.length-1;
+//        while(start<=end){
+//            int mid = start + (end-start)/2;
+//            if(nums[mid] == target ){
+//                return  mid;
+//            }
+//            if (nums[mid] >= nums[start]) {
+//                if(target >= nums[start] && target < nums[mid]){
+//                    end = mid-1;
+//                }
+//                else{
+//                    start = mid+1;
+//                }
+//            }
+//            else if(target > nums[start] && target <= nums[end])
+//                start = mid+1;
+//            else
+//                end = mid-1;
+//        }
+//        return -1;
+//    }
+//}
+
+
+
+
+//ques3. -> Median of Two Sorted Arrays
 public class Level2{
 
 }

0d417db3ab065c2cbc4698599476b5c9b66d2668
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-06-26 17:10:27 +0530

    init commit

---
+++ src/Hashing/Level1.java
@@ -0,0 +1,28 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//ques 1. -> Longest Palindrome
+//public class Level1 {
+//    public int longestPalindrome(String s){
+//        Map<Character, Integer> map = new HashMap<>();
+//        for(char ch : s.toCharArray()){
+//            map.put(ch, map.getOrDefault(ch, 0) + 1);
+//        }
+//        int ans = 0;
+//        boolean isOdd = false;
+//        for(int v:map.values()){
+//            if(v%2 == 0){
+//                ans += v;
+//            }else{
+//                ans += v-1;
+//                isOdd = true;
+//            }4
+//        }
+//        return isOdd ? ans + 1: ans;
+//    }
+//}
+
+
+Ques 2. ->

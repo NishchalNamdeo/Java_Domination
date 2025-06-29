6b537c4273c6cc3dff2a672b005e76617e635568
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-06-29 15:14:22 +0530

    init commit

--- src/Hashing/Level1.java
+++ src/Hashing/Level1.java
@@ -1,6 +1,7 @@
 package Hashing;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 //ques 1. -> Longest Palindrome
@@ -18,11 +19,26 @@ import java.util.Map;
 //            }else{
 //                ans += v-1;
 //                isOdd = true;
-//            }4
+//            }
 //        }
-//        return isOdd ? ans + 1: ans;
+//        return isOdd ? ans + 1 : ans;
 //    }
 //}
 
 
-Ques 2. ->
+//Ques 2. ->Intersection of Two Arrays
+//public class Level1 {
+//    public int[] intersection(int[] nums1, int[] nums2){
+//        HashSet<Integer> set1 = new HashSet<>();
+//        HashSet<Integer> set2 = new HashSet<>();
+//
+//        for (int i = 0; i < nums1.length; i++) {
+//          set1.add(nums1[i]);
+//        }
+//        for (int i = 0; i < nums2.length; i++) {
+//            set2.add(nums2[i]);
+//        }
+//        set1.retainAll(set2);
+//        return  set1.stream().mapToInt(Integer :: intValue).toArray();
+//    }
+//}

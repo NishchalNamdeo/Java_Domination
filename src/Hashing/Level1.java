c54689b2135e089c1ec12694d0c7c9302e67815a
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-06-30 15:37:34 +0530

    init commit

--- src/Hashing/Level1.java
+++ src/Hashing/Level1.java
@@ -42,3 +42,27 @@ import java.util.Map;
 //        return  set1.stream().mapToInt(Integer :: intValue).toArray();
 //    }
 //}
+
+
+//Ques 3. -> Subarray Sum Equals K
+//public class Level1 {
+//    public int subarraySum(int[] nums, int k){
+//        int nos = 0;
+//        Map<Integer, Integer> map = new HashMap<>();
+//        map.put(0, 1);
+//        int sum = 0;
+//        for (int i = 0; i <nums.length; i++){
+//            sum += nums[i];
+//            int diff = sum - k;
+//            if(map.containsKey(diff))
+//                nos += map.get(diff);
+//            if(map.containsKey(sum))
+//                map.put(sum, map.get(sum) + 1);
+//            else
+//                map.put(sum , 1);
+//        }
+//        return nos;
+//    }
+//}
+
+

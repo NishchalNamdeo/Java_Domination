9c84d024237e55168c66d6e0af5eea35b90be2d8
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-07-10 22:00:25 +0530

    init commit

---
+++ src/Stringbuilder/Level2.java
@@ -0,0 +1,28 @@
+package Stringbuilder;
+
+//String & StringBuilder
+
+//Ques 1. ->Valid Anagram
+//public class Level2 {
+//    public boolean isAnagram(String s, String t) {
+//    if(s.length() != t.length()) return false;
+//    int [] freq = new int [26];
+//
+//        for (int i = 0; i <s.length() ; i++) {
+//            freq[s.charAt(i) - 'a']++;
+//            freq[t.charAt(i) - 'a']--;
+//        }
+//        for (int f:freq){
+//            if(f != 0) return false;
+//        }
+//        return true;
+//    }
+//}
+
+
+//Ques 2. -> Add Binary
+public class Level2{
+    public String addBinary(String a, String b) {
+
+    }
+}

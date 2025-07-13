fd830f4074185e6a450702f70b91f74649844152
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-07-14 00:25:56 +0530

    init commit

--- src/Stringbuilder/Level2.java
+++ src/Stringbuilder/Level2.java
@@ -21,8 +21,23 @@ package Stringbuilder;
 
 
 //Ques 2. -> Add Binary
-public class Level2{
-    public String addBinary(String a, String b) {
-
-    }
-}
+//public class Level2{
+//    public String addBinary(String a, String b) {
+//       int i = a.length() - 1;
+//       int j = b.length() - 1;
+//
+//       StringBuilder sb = new StringBuilder();
+//       int carry = 0;
+//       while( i>=0 || j>=0){
+//           int sum = carry;
+//           if(i>=0) sum += Character.getNumericValue(a.charAt(i));
+//           if(j>=0) sum += Character.getNumericValue(b.charAt(j));
+//           sb.append(sum % 2);
+//           carry = sum /2;
+//           i--;
+//           j--;
+//       }
+//       if(carry ==1) sb.append(1);
+//       return sb.reverse().toString();
+//    }
+//}

8d54a67076b789b8580903bf244f1a836492de78
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-07-15 12:09:58 +0530

    init commit

--- src/Stringbuilder/Level2.java
+++ src/Stringbuilder/Level2.java
@@ -41,3 +41,40 @@ package Stringbuilder;
 //       return sb.reverse().toString();
 //    }
 //}
+
+
+//Ques 3. -> Longest Common Prefix
+//public class Level2{
+//    public String longestCommonPrefix(String[] strs){
+//        if(strs == null || strs.length == 0) return "";
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i <strs.length; i++) {
+//            char ch = strs[0].charAt(i);
+//
+//            for (int j = 1; j <strs.length; j++) {
+//                if(i>=strs[j].length() || strs[j].charAt(i) != ch)
+//                    return sb.toString();
+//            }
+//            sb.append(ch);
+//        }
+//        return sb.toString();
+//    }
+//}
+
+
+//Ques 4. ->  Rotate String
+//public class Level2{
+//    public boolean rotateString(String s, String goal){
+//        if(s.length() != goal.length()) return  false;
+//        StringBuilder sb = new StringBuilder(s);
+//        int n = s.length();
+//        while(n-- > 0){
+//            if(sb.toString().equals(goal)) return true;
+//            char ch = sb.charAt(0);
+//            sb.append(ch);
+//            sb.deleteCharAt(0);
+//        }
+//        return false;
+//    }
+//}

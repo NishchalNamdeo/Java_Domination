9721faf05581a9a2418bc339c80fc28106920953
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-07-17 20:28:12 +0530

    init commit

---
+++ src/Linkedlist/demo.java
@@ -0,0 +1,23 @@
+//package Linkedlist;
+//
+//public class demo {
+//    public static void main(String[] args) {
+//        ListNode l1 = new ListNode(10);
+//        ListNode l2 = new ListNode(20);
+//        ListNode l3 = new ListNode(30);
+//        ListNode l4 = new ListNode(40);
+//        ListNode l5 = new ListNode(50);
+//
+//        l1.next = l2;
+//        l2.next = l3;
+//        l3.next = l4;
+//        l4.next = l5;
+//
+//        ListNode temp = l1;
+//        while (temp != null){
+//            System.out.println(temp.data);
+//            temp = temp.next;
+//        }
+//    }
+//
+//}

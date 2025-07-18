bb82656bd772766b32f8dd160c1cb568ca95a2ec
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-07-18 16:18:27 +0530

    init commit

---
+++ src/Practices/Streak.java
@@ -0,0 +1,23 @@
+package Practices;
+
+//Ques 1.  -> 3136. Valid Word
+/* public class Streak {
+    public boolean isValid(String word) {
+        if(word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        String vowel = "aeiouAEIOU";
+
+        for(char c : word.toCharArray()){
+            if(Character.isLetter(c)){
+                if(vowel.indexOf(c) != -1) hasVowel = true;
+                else hasConsonant = true;
+            }else if(!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+}
+*/

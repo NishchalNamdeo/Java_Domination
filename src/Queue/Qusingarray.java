3e7410edd318a539383c4512bc5de433fc28d788
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2025-09-04 22:21:22 +0530

    init commit

--- src/Queue/Qusingarray.java
+++ src/Queue/Qusingarray.java
@@ -1,6 +1,9 @@
 package Queue;
 
-public class Qusingarray {
+
+//in array way-->
+
+/* public class Qusingarray {
     private int data[];
     private int rare;
 
@@ -35,6 +38,70 @@ public class Qusingarray {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i = 0; i <= rare; i++) {
+            sb.append(data[i]);
+            if (i < rare){
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}*/
+
+
+//in circuler array way -->
+public class Qusingarray {
+    private int data[];
+    private int rare, front, size, capacity;
+
+    public Qusingarray(int capacity) {
+        data = new int[capacity];
+        rare = front = -1;
+        size = 0;
+        this.capacity = capacity;
+    }
+    public void offer(int val){
+        if(isfull()){
+            System.out.println("full ho gya");
+            return;
+        }
+        if(rare == -1){
+            front = 0;
+        }
+        rare = (rare + 1) % capacity;
+        data[rare] = val;
+        size++;
+    }
+
+    public Integer poll(){
+        if(isempty()){
+            return  null;
+        }
+        int ans = data[front];
+        front = (front + 1) % capacity;
+        size-- ;
+        return  ans;
+    }
+
+    public Integer peek(){
+        return isempty() ? null : data[front];
+    }
+
+    public  boolean isfull(){
+        return size == capacity;
+    }
+    public boolean isempty(){
+        return  size == 0;
+    }
+
+    public String toString() {
+        if(size == 0){
+            return  "[]";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        int j = size;
+        for (int i = front; j-- > 0; i = (i + 1)% capacity) {
             sb.append(data[i]);
             if (i < rare){
                 sb.append(", ");
